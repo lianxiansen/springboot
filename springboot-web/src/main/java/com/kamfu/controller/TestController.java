4a2df14551a31b5a2a582dcfcfcdc766a266bed6
liandy <13857652373@163.com>
2020-09-01 09:58:37 +0800

    负载均衡

--- springboot-web/src/main/java/com/kamfu/controller/TestController.java
+++ springboot-web/src/main/java/com/kamfu/controller/TestController.java
@@ -3,8 +3,8 @@ package com.kamfu.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +18,13 @@ import com.kamfu.service.UserService;
 public class TestController {
 	@Autowired
 	private UserService userService;
+	
+	@Value("${config.producer.instance:0}")
+	private int instance;
 	@GetMapping("/getUser")
     @ResponseBody
     public String getUserInfo(HttpServletRequest request,String username){
-    	return AjaxResponse.success("成功",getUser(username)).toJSONString();
+    	return "[instance：" + instance + "]"+AjaxResponse.success("成功",getUser(username)).toJSONString();
     }
     private User getUser(String username) {
     	return userService.getByUsername(username);
